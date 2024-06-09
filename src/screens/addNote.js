dc5f37f4a6e4ed5ae5a24b593344be040caf267a
damarwu <hello.damarwu@gmail.com>
2024-06-09 09:43:59 +0700

    add another screens

---
+++ src/screens/addNote.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {Text} from 'react-native'
+
+const AddNote = () => <Text>
+    Tambahkan Note
+</Text>
+
+export default AddNote
---
+++ src/screens/editNote.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {Text} from 'react-native'
+
+const EditNote = () => <Text>
+    Ubah Note
+</Text>
+
+export default EditNote
